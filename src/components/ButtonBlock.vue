b10f67e0b91bcd69f23fcbff9447cc7dc814e9bc
ShmidtAlex <shmidt.alex@gmail.com>
2020-09-22 21:31:52 +0300

    route to About page added

--- src/components/ButtonBlock.vue
+++ src/components/ButtonBlock.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="q-pa-md q-gutter-sm button-section-wrapper">
-    <q-btn size="md" color="grey" text-color="black" label="About Us" />
-    <q-btn size="md" color="grey" text-color="black" label="My Bookings" />
-    <q-btn size="md" color="grey" text-color="black" label="Sign-in" />
+    <a href="/about"><q-btn size="md" color="grey" text-color="black" label="About Us" /></a>
+    <a href="#"><q-btn size="md" color="grey" text-color="black" label="My Bookings" /></a>
+    <a href="#"><q-btn size="md" color="grey" text-color="black" label="Sign-in" /></a>
   </div>
 </template>
 <script>
@@ -13,6 +13,10 @@ export default {
 <style lang="sass" scoped>
   .button-section-wrapper
     padding-right: 0px
+    display: flex
+    align-items: flex-end
+  a
+    text-decoration: none
   @media(orientation: portrait)
     .button-section-wrapper
       display: none
---
+++ src/pages/About.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-card
+      class="my-card text-white"
+      style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+    >
+      <q-card-section>
+        <div class="text-h6">About Our Company</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing, elit. Quidem harum architecto explicabo, quod facilis cumque, libero totam accusantium, omnis necessitatibus molestias provident quam odio hic cupiditate accusamus ducimus ab quo.
+  Quis pariatur maiores eveniet aspernatur itaque fugiat nobis corporis facere et recusandae atque dolor corrupti neque quod repellendus iure eos, reprehenderit praesentium tempora debitis iste ipsam, ducimus asperiores impedit doloremque.
+  Animi, minima. Minima illo, consequuntur quibusdam fugit omnis suscipit eos rerum harum. Earum totam iste expedita reprehenderit repellendus, eligendi! Laborum sapiente nisi explicabo voluptatem nihil pariatur recusandae est, fugit sit?
+  Recusandae omnis ratione, non quibusdam odit architecto aperiam tempora doloremque dicta soluta, tenetur harum iste eveniet sint exercitationem facilis minima porro maiores repellendus earum hic quasi delectus. Quaerat accusamus, suscipit.
+  Voluptatibus dolorum nam optio et delectus amet officiis rem adipisci quasi provident magni molestias aut, ullam nulla soluta nemo dolore eum in animi quia, voluptatum similique veritatis vitae qui cum!
+  Voluptatum quis iure, quisquam delectus unde natus labore neque voluptas cumque animi, sequi, architecto dolorum, temporibus cum illum esse. Eveniet, facere hic nulla harum architecto consectetur, dolorem exercitationem tempora temporibus.
+  Obcaecati nam voluptatum rerum amet ipsum minus, sit dignissimos omnis sapiente saepe recusandae minima, blanditiis vitae similique assumenda quod ut velit, beatae? Sint fugit dolorem, itaque laborum, dolor libero quaerat.
+  Aperiam, iste fuga nisi harum ab aliquid quia nemo omnis cum officiis, sint in, doloribus odio neque facere ducimus atque ad modi nulla tempore magnam. Tempore est animi iure, ipsam.
+  Unde facere, hic similique ducimus, provident, non quibusdam pariatur nulla suscipit sit dicta laborum veniam impedit, tenetur voluptatem odit. Illum magnam est, voluptate quod soluta doloremque consectetur rem debitis ipsam.
+  Corporis libero assumenda placeat eligendi ut omnis quasi, quidem. Quo, exercitationem officiis nam perferendis et, asperiores dolorum in laborum qui repudiandae nulla, consectetur aut voluptatibus, quaerat distinctio deserunt quidem. Nisi.</p>
+      </q-card-section>
+       <q-separator />
+        <q-card-actions horisontal>
+          <a href="/"><q-btn flat>Back to Search</q-btn></a>
+        </q-card-actions>
+    </q-card>
+</template>
+<style lang="sass">
+  a
+    text-decoration: none
+    color: white
+    border: 1px solid white
+    border-radius: 5px
+</style>
